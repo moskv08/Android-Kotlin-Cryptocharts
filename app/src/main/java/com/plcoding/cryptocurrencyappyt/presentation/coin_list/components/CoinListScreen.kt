1a38b1fad2b64f944d43a3f808112cc374743922
robbmoskv <fabrizio_39@hotmail.com>
2021-11-20 13:27:47 +0100

    Create coin item component and list overview

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_list/components/CoinListScreen.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_list/components/CoinListScreen.kt
@@ -1,2 +1,19 @@
 package com.plcoding.cryptocurrencyappyt.presentation.coin_list.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.plcoding.cryptocurrencyappyt.presentation.coin_list.CoinListViewModel
+import java.lang.reflect.Modifier
+
+@Composable
+fun CoinListScreen(
+    navController: NavController,
+    viewModel: CoinListViewModel = hiltViewModel(),
+){
+    val state = viewModel.state.value
+    Box(modifier = Modifier.padding()){
+
+    }
+}
