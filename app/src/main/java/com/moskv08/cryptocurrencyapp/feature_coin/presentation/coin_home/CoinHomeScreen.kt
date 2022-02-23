3aa337454efdef4637311f1d56a4d5054929ea2c
robbmoskv <fabrizio_39@hotmail.com>
2022-02-23 16:39:51 +0100

    Create Home Screen and routes

---
+++ app/src/main/java/com/moskv08/cryptocurrencyapp/feature_coin/presentation/coin_home/CoinHomeScreen.kt
@@ -0,0 +1,19 @@
+package com.moskv08.cryptocurrencyapp.feature_coin.presentation.coin_home
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun CoinHomeScreen(navController: NavController){
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Home screen")
+    }
+}
--- app/src/main/java/com/moskv08/cryptocurrencyapp/feature_coin/presentation/utli/Screen.kt
+++ app/src/main/java/com/moskv08/cryptocurrencyapp/feature_coin/presentation/utli/Screen.kt
@@ -3,4 +3,5 @@ package com.moskv08.cryptocurrencyapp.feature_coin.presentation.utli
 sealed class Screen(val route: String) {
     object CoinListScreen: Screen("coin_list_screen")
     object CoinDetailScreen: Screen("coin_detail_screen")
+    object CoinHomeScreen: Screen("home")
 }
